1142138d9b30014a5a118b45829296c79bbd5d50
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-02-28 17:18:49 -0300

    API-CLIENT / Estiliza a tag Container

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,23 @@
-import {FaGithubAlt, FaPlus} from "react-icons/fa";
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 import React from 'react';
 import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
   return (
     <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositorios
+      </h1>
 
-      <h1><FaGithubAlt />Repositorios</h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="adicionar repositorio" />
+
+        <SubmitButton disable>
+          <FaPlus color="FFF" size={14} />
+        </SubmitButton>
+      </Form>
     </Container>
-    <Form onSubmit={()=> {}}>
-      <input type="text" placeholder="adicionar repositorio" />
-    </Form>
-    <SubmitButton disable>
-      <FaPlus color="FFF" size={14}/>
-    </SubmitButton>
   );
 }
 
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,7 +1,22 @@
 import styled from 'styled-components';
 
 export const Container = styled.h1`
-  color: #fff;
+  max-width: 700px;
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 30px;
+  padding: 30px;
+  margin: 80px auto;
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    svg {
+      margin-right: 10px;
+    }
+  }
 `;
 export const Form = styled.form``;
 export const SubmitButton = styled.button``;
