21667ca505852d592266038cd636dd3e9397077a
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-02-28 17:01:50 -0300

    API-CLIENT / Cria tags em styles.js

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,20 @@
+import {FaGithubAlt, FaPlus} from "react-icons/fa";
 import React from 'react';
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
-  return <Title>Hello World</Title>;
+  return (
+    <Container>
+
+      <h1><FaGithubAlt />Repositorios</h1>
+    </Container>
+    <Form onSubmit={()=> {}}>
+      <input type="text" placeholder="adicionar repositorio" />
+    </Form>
+    <SubmitButton disable>
+      <FaPlus color="FFF" size={14}/>
+    </SubmitButton>
+  );
 }
 
 export default Main;
